e24a9956dc62b8fa1f17c02c4f0ac421b083c134
AleRojas27 <alejandro.rrc99@hotmail.com>
2022-02-16 05:39:15 -0600

    Avance de cabezote - Resultados de busqueda

---
+++ vista/modulos/cabezote.html.php
@@ -0,0 +1,24 @@
+<div class="container-fluid cabezote">
+
+    <div class="row">
+
+        <div class="col-2">
+
+            <div class="_icono">
+
+                <i class="fas fa-bars icono"></i>
+
+            </div>
+
+        </div>
+
+        <div class="col-5">
+
+        </div>
+
+        <div class="col-5"></div>
+
+    </div>
+
+
+</div>
